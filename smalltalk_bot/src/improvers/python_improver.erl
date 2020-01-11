de1776ff2e756300375f41ecde3c5ab92f09d5a7
jgapski <jann.gapski@gmail.com>
2020-01-11 21:56:28 +0100

    basic messages analysic

--- smalltalk_bot/src/improvers/python_improver.erl
+++ smalltalk_bot/src/improvers/python_improver.erl
@@ -4,5 +4,11 @@
 
 -spec improve(map()) -> [binary()].
 improve(#{<<"msg">> := Msg}) ->
-    [
-    ].
+        check_it(Msg).
+
+check_it(Msg) ->
+        Pat = "python src/python/messages_analysic.py " ++ binary_to_list(Msg),
+        R = os:cmd(Pat),
+        logger:error("~p", [R]),
+        R.
+
--- smalltalk_bot/src/python/messages_analysic.py
+++ smalltalk_bot/src/python/messages_analysic.py
@@ -1,10 +1,11 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from operator import itemgetter 
+import sys
 
-def sentiment_analyzer_scores(map):
+def sentiment_analyzer_scores(sentence):
     var_beh = ""
     analyser = SentimentIntensityAnalyzer()
-    sentence = map.get("msg")
+    #sentence = map.get("msg")
     score = analyser.polarity_scores(sentence)
     #print("{:-<40} {}".format(sentence, str(score)))
     #print(sentence, str(score))
@@ -21,3 +22,9 @@ def sentiment_analyzer_scores(map):
         #print("Neutral") 
     return var_beh
 
+
+arguments = str(sys.argv)
+a_msg = arguments.replace("['src/python/messages_analysic.py', '", "")
+msg = a_msg.replace("']", "")
+
+print(sentiment_analyzer_scores(msg))
