9441d511e60ae0fbc1f26695810e2f23b936e3c3
jgapski <jann.gapski@gmail.com>
2020-01-11 22:05:06 +0100

    small changes in example_request.sh

--- smalltalk_bot/example_request.sh
+++ smalltalk_bot/example_request.sh
@@ -4,6 +4,14 @@ curl  -X POST \
 -d '{"from":"Vlad", "channel_name":"random", "msg":"Hello, how are you?"}' \
 http://localhost:8080
 
+curl  -X POST \
+-d '{"from":"Vlad", "channel_name":"random", "msg":"Yes, you are awsome"}' \
+http://localhost:80804
+
+curl  -X POST \
+-d '{"from":"Vlad", "channel_name":"random", "msg":"No, fuck"}' \
+http://localhost:8080
+
 Response:
 
 {
