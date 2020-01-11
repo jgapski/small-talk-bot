df34b1b2094928f89d5f80af9d37de84deedad31
Aleksander Lisiecki <sebaczechu123@gmail.com>
2020-01-11 23:12:30 +0100

    Change json lib

--- smalltalk_bot/src/handlers/improve_outgoing_messages.erl
+++ smalltalk_bot/src/handlers/improve_outgoing_messages.erl
@@ -5,13 +5,13 @@
 init(Req0, Opts) ->
 
     {ok, Data, Req1} = cowboy_req:read_body(Req0),
-    DecodedMsg = jiffy:decode(Data, [return_maps]),
+    DecodedMsg = jsone:decode(Data),
 
     Suggestions =
         erlang_improver:improve(DecodedMsg)
         ++ python_improver:improve(DecodedMsg),
 
-    Response = jiffy:encode(#{ suggestions => Suggestions}),
+    Response = jsone:encode(#{ suggestions => Suggestions}),
 	Req2 = cowboy_req:reply(200, #{
 		<<"content-type">> => <<"text/plain">>
 	}, Response, Req1),
