a5ff7e4df729cb93880165aaaab66975da399512
Aleksander Lisiecki <sebaczechu123@gmail.com>
2020-01-12 06:38:05 +0100

    Add message rate limitatin suggestion

--- smalltalk_bot/src/improvers/erlang_improver.erl
+++ smalltalk_bot/src/improvers/erlang_improver.erl
@@ -4,7 +4,11 @@
 
 improve(#{<<"from">> := From, <<"msg">> := Msg}) ->
     [] ++
-    save_and_detect_ASAPs(From, Msg).
+    save_and_detect_ASAPs(From, Msg) ++
+    limit_message_rate(From, Msg).
 
 save_and_detect_ASAPs(From, Msg) ->
-    db:add_msg(From, Msg).
+    asap_rate_counter:add_msg(From, Msg).
+
+limit_message_rate(From, Msg) ->
+    msg_rate_limitation_counter:add_msg(From, Msg).
